a7090aa33b5c8c9b705296c034333f583187fea0
Dr.jacky <dr_jacky_2005@yahoo.com>
2018-04-27 10:39:07 +0430

    NetworkUtils created.

---
+++ app/src/main/kotlin/ir/hosseinabbasi/payconiq/utils/NetworkUtils.kt
@@ -0,0 +1,18 @@
+package ir.hosseinabbasi.payconiq.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+
+/**
+ * Created by Dr.jacky on 2018/04/27.
+ */
+class NetworkUtils constructor(){
+
+    companion object {
+        fun isNetworkConnected(context: Context): Boolean {
+            val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val activeNetwork = cm.activeNetworkInfo
+            return activeNetwork != null && activeNetwork.isConnectedOrConnecting
+        }
+    }
+}
